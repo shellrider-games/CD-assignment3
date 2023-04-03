52f48c61cbf500470b3a1bab34d518b1a324409b
Georg Becker <Georg.Becker@gmx.at>
2023-04-03 15:27:00 +0200

    Implements tests for cuboid Surface

--- GeometryLibraryTests/CuboidTests.cs
+++ GeometryLibraryTests/CuboidTests.cs
@@ -62,5 +62,23 @@ namespace GeometryLibraryTests
             });
             Assert.That(cube.SurfaceArea(), Is.EqualTo(6f));
         }
+
+        [Test]
+        public void Test_Surface_Cuboid()
+        {
+            Cuboid cuboid = new Cuboid(new Vector3[]{
+                new Vector3(-0.858227f,-0.907245f,0.756414f),
+                new Vector3(2.37286f,-0.907245f,0.672945f),
+                new Vector3(2.08237f,1.06583f,1.16701f),
+                new Vector3(-1.05237f,1.06583f,1.16701f),
+                new Vector3(-0.943518f,-0.907245f,-1.04674f),
+                new Vector3(2.16075f,-0.907245f,-1.04674f),
+                new Vector3(1.30668f,1.09312f,-1.68679f),
+                new Vector3(-1.13085f,1.09312f,-1.68679f),
+            });
+            Assert.That(MathF.Round(cuboid.SurfaceArea(),2), Is.EqualTo(35.29f));
+        }
+        
     }
+   
 }
